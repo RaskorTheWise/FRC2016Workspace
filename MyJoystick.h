bbcf3aa271295eccb1e8abcf9e0f54be3e38e793
RaskorTheWise <enjaygee40@yahoo.com>
2016-02-16 16:39:05 -0800

    Create MyJoystick.h

---
+++ MyJoystick.h
@@ -0,0 +1,33 @@
+/*
+ * MyJoystick.h
+ */
+#include "WPILib.h"
+
+#ifndef SRC_MYJOYSTICK_H_
+#define SRC_MYJOYSTICK_H_
+
+class MyJoystick {
+private:
+	Joystick* m_Joystick;
+public:
+	MyJoystick();
+	virtual ~MyJoystick();
+
+	void init(Joystick* theJoystick);
+
+	void readJoystick();
+
+	bool readButton(int buttonNumber);
+
+	float checkLeftStickX();
+
+	float checkLeftStickY();
+
+	float checkRightStickX();
+
+	float checkRightStickY();
+
+	int getPOV();
+};
+
+#endif /* SRC_MYJOYSTICK_H_ */
